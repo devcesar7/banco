d615617c15d53d7dc9902abaa7d11dcb4179f40c
devcesar7 <devcesaraugusto97@gmail.com>
2025-04-08 15:40:18 -0300

    criando mais metodos da classe

--- conta.py
+++ conta.py
@@ -15,9 +15,17 @@ class Conta:
         self.__saldo += valor
         print(f"Deposito de {valor} na conta de {self.__titular}")
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def sacar(self, valor):
-        self.__saldo -= valor
-        print(f"Saque de {valor} da conta de {self.__titular}")
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            print(f"Saque de {valor} da conta de {self.__titular}")
+        else:
+            print("ERRO!!!")
+            print(f"O valor {valor}, ultrapassou o limite de {self.__limite}")
 
     def transferir(self, valor, destino):
         self.sacar(valor)
@@ -38,3 +46,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
